d017d03db5331ede09fe50fc54b02641f09b9c44
Arpit Paliwal <apaliwal@localhost.localdomain>
2020-10-21 20:27:44 +0530

    added some sample test

--- test-specs/selenoid/basic.specs.js
+++ test-specs/selenoid/basic.specs.js
@@ -1,7 +1,13 @@
-
+const { assert } = require('chai');
+const Page = require('../../test/Components/pageobjects/secure.page');
 describe('webdriver.io page', () => {
     it('should have the right title', () => {
         browser.url('https://webdriver.io')
+        browser.maximizeWindow();
         expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
+        Page.clickApiLink();
+        assert.strictEqual(Page.verifyBrowserMethod(), true, "Browser method should be present");
+        Page.clickBrowserMethod();
+        assert.strictEqual(Page.verifyEditButton(), true, "Edit button should be present");
     })
 })
--- test/Components/pageobjects/secure.page.js
+++ test/Components/pageobjects/secure.page.js
@@ -8,6 +8,33 @@ class SecurePage extends Page {
      * define selectors using getter methods
      */
     get flashAlert () { return $('#flash') }
+    get APIlink() { return $('//a[text()="API"]');}
+    get browserMethod() { return $('//a[text()="executeAsync"]');}
+    get editButton() {return $('//a[text()="Edit"]')}
+    clickApiLink(){
+        this.APIlink.waitForClickable({
+            timeout: 80000
+        });
+        this.APIlink.click();
+    }
+
+    verifyBrowserMethod(){
+        this.browserMethod.waitForDisplayed({
+            timeout: 80000
+        });
+        return this.browserMethod.isDisplayed();
+    }
+
+    clickBrowserMethod(){
+        this.browserMethod.waitForClickable({
+            timeout: 80000
+        });
+        this.browserMethod.click();
+    }
+    
+    verifyEditButton(){
+        return this.editButton.isDisplayed();
+    }
 }
 
 module.exports = new SecurePage();
